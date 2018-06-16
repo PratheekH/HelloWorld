c8d7a5b645ef4ec7b5d0a79778eab31015c18dc0
PratheekH <pratheekharalike993@email.com>
2018-06-16 22:28:21 +0530

    Added logic

--- Hello.java
+++ Hello.java
@@ -2,7 +2,14 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("HelloWorld!");
-System.out.println("Sample");
+	System.out.println("HelloWorld!");
+	for(int i=1;i<=5;i++)
+		{
+			for(int j=0;j<i;j++)
+				{
+					System.out.print("*");
+				}
+			System.out.println(" ");
+		}
 }
 }
