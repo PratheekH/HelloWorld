f5e53d62595920eaf32fcc8c7e89b5e7c71916e2
PratheekH <pratheekharalike993@email.com>
2018-06-16 22:47:30 +0530

    new logic

--- Hello.java
+++ Hello.java
@@ -3,13 +3,13 @@ class Hello
 public static void main(String args[])
 {
 	System.out.println("HelloWorld!");
-	for(int i=1;i<=5;i++)
+	for(int i=5;i>=1;i--)
 		{
+			System.out.println("");
 			for(int j=0;j<i;j++)
 				{
 					System.out.print("*");
 				}
-			System.out.println(" ");
 		}
 }
 }
